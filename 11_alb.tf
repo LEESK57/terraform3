09e6fec4347573d3655dd5a09fb63184ac14d85e
sklee <lsk940507@gmail.com>
2021-11-26 14:37:31 +0900

    step1

--- 11_alb.tf
+++ 11_alb.tf
@@ -3,8 +3,12 @@ resource "aws_lb" "sdkim_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sdkim_sg.id]
-  subnets            = [aws_subnet.sdkim_puba.id,aws_subnet.sdkim_pubc.id]
+  subnets            = [aws_subnet.sdkim_puba.id, aws_subnet.sdkim_pubc.id]
   tags = {
     "Name" = "sdkim-alb"
   }
+}
+
+output "dns_name" {
+  value = aws_lb.sdkim_alb.dns_name
 }
