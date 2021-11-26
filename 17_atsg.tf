3cc7301ea99e196f0528ce695469b953753d0777
sklee <lsk940507@gmail.com>
2021-11-26 16:13:31 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -6,9 +6,9 @@ resource "aws_placement_group" "sdkim_pg" {
 resource "aws_autoscaling_group" "sdkim_atsg" {
   name                      = "sdkim-atsg"
   min_size                  = 2
-  max_size                  = 8
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
+  max_size                  = 10
+  health_check_grace_period = 60
+  health_check_type         = "EC2"
   desired_capacity          = 2
   force_delete              = true
   launch_configuration      = aws_launch_configuration.sdkim_lacf.id
