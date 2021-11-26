46543faadd6461dabd358550a7e58a55886812f7
sklee <lsk940507@gmail.com>
2021-11-26 17:19:57 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -1,7 +1,8 @@
-resource "aws_placement_group" "sdkim_pg" {
+/*resource "aws_placement_group" "sdkim_pg" {
   name     = "sdkim-pg"
   strategy = "cluster"
 }
+*/
 
 resource "aws_autoscaling_group" "sdkim_atsg" {
   name                      = "sdkim-atsg"
@@ -12,6 +13,6 @@ resource "aws_autoscaling_group" "sdkim_atsg" {
   desired_capacity          = 2
   force_delete              = true
   launch_configuration      = aws_launch_configuration.sdkim_lacf.id
-  placement_group           = aws_placement_group.sdkim_pg.id
+  #placement_group           = aws_placement_group.sdkim_pg.id
   vpc_zone_identifier       = [aws_subnet.sdkim_puba.id, aws_subnet.sdkim_pubc.id]
 }
